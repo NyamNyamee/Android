fc29f656f4db2bdb9035885ce2df3436f9685ed3
inwoo <jopelee2@gmail.com>
2020-11-10 17:47:41 +0900

    SYM main, login activity added

--- Ex01/app/src/main/java/com/example/ex01/LoginActivity.java
+++ Ex01/app/src/main/java/com/example/ex01/LoginActivity.java
@@ -5,14 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
+    EditText idET, pwET;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
+
+        idET = findViewById(R.id.idET);
+        pwET = findViewById(R.id.pwET);
     }
 
     public void moveToMain(View view) {
@@ -21,9 +27,9 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent1);
     }
 
-    public void moveToLogin(View view) {
-        Toast.makeText(this, "Move to login page", Toast.LENGTH_SHORT).show();
-        Intent intent1 = new Intent(this, LoginActivity.class);
-        startActivity(intent1);
+    public void clearBtn(View view) {
+        Toast.makeText(this, idET.getText().toString() + " - " + pwET.getText().toString(), Toast.LENGTH_SHORT).show();
+        idET.setText("");
+        pwET.setText("");
     }
 }
