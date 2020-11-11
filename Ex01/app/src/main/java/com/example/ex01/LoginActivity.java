8664dd725d0ba6f9332ac702ea5d04da75d7f7e2
inwoo <jopelee2@gmail.com>
2020-11-11 14:38:46 +0900

    Temporary login added
    Needed login function

--- Ex01/app/src/main/java/com/example/ex01/LoginActivity.java
+++ Ex01/app/src/main/java/com/example/ex01/LoginActivity.java
@@ -3,7 +3,9 @@ package com.example.ex01;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -28,8 +30,54 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     public void clearBtn(View view) {
-        Toast.makeText(this, idET.getText().toString() + " - " + pwET.getText().toString(), Toast.LENGTH_SHORT).show();
+        // Toast.makeText(this, idET.getText().toString() + " - " + pwET.getText().toString(), Toast.LENGTH_SHORT).show();
         idET.setText("");
         pwET.setText("");
+    }
+
+    // 앱이 일시정지될 때 값을 공유메모리에 저장해놓음
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // myData라는 공유메모리 생성(내 앱에서만 접근가능하는 모드)
+        SharedPreferences preferences = getSharedPreferences("myData", MODE_PRIVATE);
+        // 공유영역을 편집하는 편집기 객체생성
+        SharedPreferences.Editor editor = preferences.edit();
+        // 편집기에 data라는 text데이터를 저장
+        editor.putString("id", idET.getText().toString());
+        editor.putString("pw", pwET.getText().toString());
+        // 적용
+        editor.commit();
+    }
+
+    // 앱을 다시 켰을 때 값을 불러와 작업
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // myData라는 공유메모리 불러오기
+        SharedPreferences preferences = getSharedPreferences("myData", MODE_PRIVATE);
+        // 공유메모리영역에 id, pw라는 데이터가 존재한다면
+        if (preferences != null && preferences.contains("id") && preferences.contains("pw")) {
+            // 데이터(데이터, 기본값)를 읽어 editText에 저장
+            String id = preferences.getString("id", "");
+            String pw = preferences.getString("pw", "");
+            idET.setText(id);
+            pwET.setText(pw);
+        }
+    }
+
+    public void signIn(View view) {
+        // 임시 로그인을 위해 아이디와 비밀번호가 같을 때에만 로그인하도록
+        String id = idET.getText().toString();
+        String pw = pwET.getText().toString();
+        if (id.trim().length() != 0 && id != null && id.equals(pw)) {
+            Toast.makeText(this, "Hello " + id + "~!", Toast.LENGTH_SHORT).show();
+            // Log.d("loginTag", id + "/" + pw);
+            finish();
+        } else {
+            // 실패시 로그인실패 토스트 출력
+            Toast.makeText(this, "login failed", Toast.LENGTH_SHORT).show();
+        }
     }
 }
